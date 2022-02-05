b7d84530cc344c27ce1093d43cc5c30579dbd9a2
katBaack <deividpedrosa04@gmail.com>
2022-02-05 20:15:15 -0300

    stilos e inicio de responsividade

--- public/js/header.js
+++ public/js/header.js
@@ -1,2 +1,24 @@
+window.addEventListener('load',() => {
+    const ofertas = document.getElementById("ofertas");
 
+    ofertas.addEventListener("mouseover", function () {
+        this.classList.add("btnHeader");
+        this.style.cursor = "pointer";
+      });
+    
+      ofertas.addEventListener("click", function () {
+          
+          if (window.location.href != "http://localhost:3000/home") {
+              window.location.replace("http://localhost:3000/home");
+        }
+        
+          if(window.location.href == "http://localhost:3000/home") {
+            window.scroll({
+                top: 800,
+                behavior: "smooth",
+            })
+        }
+      });
+       
+})
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 const pageAtual = window.location.href;
 const caixas = document.getElementsByClassName("caixa");
-const ofertas = document.getElementById("ofertas");
 
 for (let caixa of caixas) {
     caixa.addEventListener("mouseover", function () {
@@ -11,26 +10,8 @@ for (let caixa of caixas) {
   });
 
   caixa.addEventListener("mouseout", function () {
-    this.style.transform = "scale(1)";
+    this.style.transform = "";
     this.children[1].children[0].style.color = "";
     this.children[1].children[0].style.fontWeight = "";
   });
 }
-
-ofertas.addEventListener("mouseover", function () {
-  this.classList.add("btnHeader");
-  this.style.cursor = "pointer";
-});
-ofertas.addEventListener("click", function () {
-    
-    if (window.location.href != "http://localhost:3000/home") {
-        window.location.replace("http://localhost:3000/home");
-  }
-  
-    if(window.location.href == "http://localhost:3000/home") {
-        window.scroll({
-            top: 800,
-            behavior: "smooth",
-        })
-  }
-});
